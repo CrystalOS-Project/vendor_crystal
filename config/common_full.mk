a19f61502e83bbc73699ef28b6d757a2bd5ae51b
HyperN00B <bence.tari@gmail.com>
2021-09-16 21:48:45 +0200

    Remove Fonts as those are called from build folder

--- config/common_full.mk
+++ config/common_full.mk
@@ -3,15 +3,9 @@ $(call inherit-product, vendor/crystal/config/common_mobile.mk)
 
 PRODUCT_SIZE := full
 
-# Include {Lato,Rubik} fonts
-$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
-$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
-
 # Fonts
 PRODUCT_PACKAGES += \
-    fonts_customization.xml \
-    CrystalLatoFont \
-    CrystalRubikFont
+    fonts_customization.xml
 
 # Recorder
 PRODUCT_PACKAGES += \
