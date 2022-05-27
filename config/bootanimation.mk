7f9f8c8b3a00fe483e93252690e9271917046873
Tari Bence <bence.tari@gmail.com>
2022-05-28 01:04:58 +0200

    bootanimation: Use Dark Pixel 2 bootanimation and add 1440p to supported resolutions

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -1,7 +1,10 @@
 # Boot Animation
 
+# 1440p
+ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+PRODUCT_COPY_FILES += vendor/crystal/prebuilt/common/bootanimation/1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 # 1080p
-ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
 PRODUCT_COPY_FILES += vendor/crystal/prebuilt/common/bootanimation/1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 # 720p
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
@@ -11,5 +14,10 @@ else ifeq ($(TARGET_BOOT_ANIMATION_RES),480)
 PRODUCT_COPY_FILES += vendor/crystal/prebuilt/common/bootanimation/480.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 # Default to 720p if the device does not set the flag.
 else
-PRODUCT_COPY_FILES += vendor/crystal/prebuilt/common/bootanimation/720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
+        $(warning "CrystalOS: TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+    else
+        $(warning "CrystalOS: Current bootanimation res is not supported, forcing 1080p")
+    endif
+    PRODUCT_COPY_FILES += vendor/crystal/prebuilt/common/bootanimation/1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 endif
