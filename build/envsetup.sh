4a57eea9dade471b506f53ffc241c5859af6710f
HyperN00B <bence.tari@gmail.com>
2024-04-19 22:26:57 +0200

    vendor: Use lunch for Crystalize method instead of breakfast

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -56,41 +56,15 @@ function mk_timer()
 
 function Crystalize()
 {
-    breakfast $*
+    lunch $*
     if [ $? -eq 0 ]; then
         m crystal -j$(nproc --all)
     else
-        echo "No such item in brunch menu. Try 'breakfast'"
+        echo "No such item in Crystalize menu. Try 'lunch'"
         return 1
     fi
     return $?
 }
-
-function breakfast()
-{
-    target=$1
-    local variant=$2
-
-    if [ $# -eq 0 ]; then
-        # No arguments, so let's have the full menu
-        lunch
-    else
-        if [[ "$target" =~ -(user|userdebug|eng)$ ]]; then
-            # A buildtype was specified, assume a full device name
-            lunch $target
-        else
-            # This is probably just the Crystal model name
-            if [ -z "$variant" ]; then
-                variant="userdebug"
-            fi
-
-            lunch crystal_$target-$variant
-        fi
-    fi
-    return $?
-}
-
-alias bib=breakfast
 
 function flash()
 {
